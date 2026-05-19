47f07cc07b0587d7f800cc6f6a7079acc88ef678
Senthalan S <senthalan2@gmail.com>
2026-05-19 16:58:31 +0530

    Example updated

--- example/App.tsx
+++ example/App.tsx
@@ -1,4 +1,11 @@
-import { Button, PermissionsAndroid, Platform, View } from 'react-native';
+import {
+  PermissionsAndroid,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { useEffect, useRef } from 'react';
 import StickyNotification, {
   ActionPressEvent,
@@ -45,11 +52,22 @@ const App = () => {
         closeOnAction: true,
         showDivider: false,
         openAppOnAction: true,
-        actionBackground: '#b3e9c6',
-        actionBorderRadius: 30,
+        actionLabelColor: '#2200ff',
         actionIconTint: '#1DB954',
-        actionLabelColor: '#0d43f5',
-
+        actionSpacing: 4,
+        actionIconSpacing: 15,
+        actionBorderRadius: 10,
+        actionsContainerBackground: '#f59074',
+        collapsedActions: [
+          { id: 'prev', title: 'Prev', icon: 'ic_skip_previous' },
+          { id: 'pause', title: 'Pause', icon: 'ic_pause' },
+          { id: 'next', title: 'Next', icon: 'ic_skip_next' },
+          { id: 'next', title: 'Next', icon: 'ic_skip_next' },
+          { id: 'next', title: 'Next', icon: 'ic_skip_next' },
+        ],
+        showLabelsInCollapsed: false, // icon-only in collapsed (default)
+        buttonsPerRow: 5,
+        containerBorderRadius: 10,
         actions: [
           {
             id: 'prev',
@@ -78,13 +96,31 @@ const App = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Start notification" onPress={start} />
-      <Button
-        title="Stop notification"
+      <TouchableOpacity activeOpacity={0.6} style={styles.btn} onPress={start}>
+        <Text style={styles.btnText}>Start Service</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.6}
+        style={styles.btn}
         onPress={() => StickyNotification.stopService()}
-      />
+      >
+        <Text style={styles.btnText}>Stop Service</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  btn: {
+    padding: 10,
+    borderRadius: 15,
+    marginVertical: 15,
+    backgroundColor: '#2eb6b6',
+  },
+  btnText: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
